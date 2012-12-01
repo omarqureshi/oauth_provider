09b22438e78091c0439593fb57ecd3206ed5495d
Omar Qureshi <omarqureshi@Omars-MacBook-Pro.local>
2012-12-01 12:51:09 +0000

    only validate password length if we need to

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email
   validates_uniqueness_of :email
-  validates_length_of :password, :minimum => 6
+  validates_length_of :password, :minimum => 6, :if => Proc.new {|u| u.password_digest.blank? || !u.password.blank? }
   
   attr_accessible :email, :password, :password_confirmation
 end
