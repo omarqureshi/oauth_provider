3e71b659eb2ae29642425a57cfa65f4f87f5e8f0
Omar Qureshi <omar@omarqureshi.net>
2012-12-10 00:23:21 +0100

    add provider and uid to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email
   validates_uniqueness_of :email
-  validates_length_of :password, :minimum => 6, :if => Proc.new {|u| u.password_digest.blank? || !u.password.blank? }
-  
+  validates_length_of :password, :minimum => 6, :if => Proc.new {|u| u.password_digest.blank? || !u.password.blank? || !(u.uid && u.provider) }
   attr_accessible :email, :password, :password_confirmation
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121121094854) do
+ActiveRecord::Schema.define(:version => 20121209232105) do
 
   create_table "access_grants", :force => true do |t|
     t.integer  "resource_owner_id", :null => false
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(:version => 20121121094854) do
     t.boolean  "admin",           :default => false, :null => false
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
+    t.string   "uid"
+    t.string   "provider"
   end
 
 end
